ad280ec66d50d30a2b3b52d4d29302305cb809ce
Mirko <mirko.kunze@web.de>
2020-02-03 22:16:13 +0100

    first test

---
+++ script/main.js
@@ -0,0 +1,43 @@
+
+var h_scroll = 1000;
+var scrolled_up = true;
+
+function dqqresize(){
+  var w = window.visualViewport.width-5;
+  var h = window.visualViewport.height-5;
+
+  console.log(w + " " + h)
+  var h_i = h*0.06;
+  h_scroll = h-h_i;
+
+  document.getElementById('i_img').style.height = h_i + "px";
+  document.getElementById('i_img').style.width = w + "px";
+
+  document.getElementById('poem_div').style.height = h_scroll + "px";
+  document.getElementById('poem_div').style.width = w + "px";
+  document.getElementById('poem_img').style.height = "auto";
+  document.getElementById('poem_img').style.width = w + "px";
+
+  document.getElementById('map_div').style.height = h + "px";
+  document.getElementById('map_div').style.width = w + "px";
+  document.getElementById('map_img').style.height = "auto";
+  document.getElementById('map_img').style.width = w + "px";
+}
+
+function dqqscroll(){
+  scrolled_up = !scrolled_up;
+  if(scrolled_up){
+    document.getElementById('scroll_div').classList.remove("pos_down");
+    document.getElementById('scroll_div').classList.add("pos_up");
+  }
+  else{
+    document.getElementById('scroll_div').classList.remove("pos_up");
+    document.getElementById('scroll_div').classList.add("pos_down");
+  }
+}
+
+window.addEventListener('resize', dqqresize);
+window.setTimeout(function(){
+  dqqresize()
+  dqqscroll()
+}, 1000)
